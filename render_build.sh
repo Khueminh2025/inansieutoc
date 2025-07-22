404213c92adb54f1a200bdcbd839db2ee5ca22d4
Khueminh2025 <khueminh2025@gmail.com>
2025-07-22 18:06:43 +0700

    Add Tailwind build step

--- render_build.sh
+++ render_build.sh
@@ -3,8 +3,13 @@
 # Apply migrations
 python manage.py migrate --noinput
 
+# Install Node packages (Tailwind)
+npm install
+
+npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
 # Collect static files
 python manage.py collectstatic --noinput
 
+
 # Import service data (nếu chưa có)
-python manage.py import_services || true  # Không crash nếu lỗi
+# python manage.py import_services || true  # Không crash nếu lỗi
