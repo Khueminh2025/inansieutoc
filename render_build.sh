92eddcb723a3179e12cfc55d6b6da78f94f7a3d1
Khueminh2025 <khueminh2025@gmail.com>
2025-07-22 18:29:11 +0700

    All

--- render_build.sh
+++ render_build.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-# Apply migrations
-python manage.py migrate --noinput
-
-# Install Node packages (Tailwind)
-npm install
-
+echo "Building Tailwind CSS..."
 npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
-# Collect static files
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Running migrations..."
+python manage.py migrate
 
 # Import service data (nếu chưa có)
 # python manage.py import_services || true  # Không crash nếu lỗi
