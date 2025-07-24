ba7c6afa111f0f5b1635baa40c11bad36202c27f
Khueminh2025 <khueminh2025@gmail.com>
2025-07-25 01:20:15 +0700

    Add gdgd

--- render_build.sh
+++ render_build.sh
@@ -12,5 +12,5 @@ python manage.py migrate
 echo "==> Creating superuser"
 python manage.py create_admin
 
-Import service data (nếu chưa có)
+# Import service data (nếu chưa có)
 python manage.py import_all_print_data || true  # Không crash nếu lỗi
